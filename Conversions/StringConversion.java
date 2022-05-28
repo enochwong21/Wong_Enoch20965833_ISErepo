dec736decde76ce88b83766a958f30b5bfb05df0
Enoch Wong <700043445@student.curtin.edu.my>
2022-05-28 02:41:37 -0700

    Added a new method called removeAllDigit, is to remove any numeric values in a given string

--- Conversions/StringConversion.java
+++ Conversions/StringConversion.java
@@ -57,4 +57,27 @@ public class Conversion
 		}
 		return sb;
 	}
+	
+	public static String removeAllDigit(String str)
+    	{
+        // Converting the given string
+        // into a character array
+        char[] charArray = str.toCharArray();
+        String result = "";
+  
+        // Traverse the character array
+        for (int i = 0; i < charArray.length; i++) 
+        {
+  
+            // Check if the specified character is not digit
+            // then add this character into result variable
+            if (!Character.isDigit(charArray[i])) 
+            {
+                result = result + charArray[i];
+            }
+        }
+        // Return result
+        return result;
+    	}
+	
 }
