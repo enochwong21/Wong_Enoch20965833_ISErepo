aedf16a0c3c094643b74847921e4294213261f3e
Enoch Wong <700043445@student.curtin.edu.my>
2022-05-28 02:38:25 -0700

    Added a new method called toUppercase, to let user input become uppercase.

--- Conversions/StringConversion.java
+++ Conversions/StringConversion.java
@@ -23,5 +23,26 @@ public class Conversion
 		}while(repeat);
 	}
 	
+	public static String toUpperCase(String userInput)
+	{
+		char returnMain = 0;
+		Scanner sc = new Scanner(System.in);
+		boolean repeat = true;
+		do
+		{
+			if(userInput == null || userInput.isEmpty())
+			{
+				System.out.print("Please enter a string: ");
+				userInput = sc.nextLine();
+			}
+			else 
+			{				
+				System.out.println("\nInteger in String is: " + checkNumericInString(userInput));
+				System.out.println("Lowercase to Uppercase : " + removeAllDigit(userInput).toUpperCase() + "\n");
+				repeat = false;
+			}		
+		}while(repeat);
+	}
+	
 	
 }
